f78d1ec81f5c669d1aa674fac95b5a3718459d83
ozz <ajaycrh710@gmail.com>
2019-03-20 17:45:26 +0530

    intial commit

---
+++ Assets/aaa.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aaa : MonoBehaviour {
+    
+	public Animator anime;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("l") && anime.GetCurrentAnimatorStateInfo(0).IsName("none"))
+		{
+			anime.Play("open");
+		
+		}
+		else if(Input.GetKeyDown("l") && anime.GetCurrentAnimatorStateInfo(0).IsName("close"))
+		{
+			anime.Play("open");
+		}
+		else if(Input.GetKeyDown("l") && anime.GetCurrentAnimatorStateInfo(0).IsName("open"))
+		{
+			anime.Play("none");
+		}
+	}
+}
